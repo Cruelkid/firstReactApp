070bb4a75f1fb11f9402551db928475d23dbdf03
Cruelkid <sonofammit@gmail.com>
2021-07-24 12:21:57 +0300

    added expenses filter

---
+++ src/components/Expenses/ExpensesFilter.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './ExpensesFilter.css';
+
+const ExpensesFilter = (props) => {
+    const dropdownChangeHandler = (event) => {
+        let chosenYear = event.target.value;
+        props.onYearChange(chosenYear);
+    };
+console.log(props.initialYear);
+    return (
+        <div className='expenses-filter'>
+            <div className='expenses-filter__control'>
+            <label>Filter by year</label>
+            <select onChange={dropdownChangeHandler} defaultValue={props.initialYear}>
+                <option value='2022'>2022</option>
+                <option value='2021'>2021</option>
+                <option value='2020'>2020</option>
+                <option value='2019'>2019</option>
+            </select>
+            </div>
+        </div>
+    );
+};
+
+export default ExpensesFilter;
--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -1,19 +1,30 @@
+import { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 import "./ExpensesList.css";
 
 const ExpensesList = (props) => {
+    const [chosenYear, setChosenYear] = useState('2019');
+
+    const changeYearHandler = (enteredYear) => {
+        setChosenYear(enteredYear);
+    };
+
     return (
-        <Card className="expenses">
-            {props.expenses.map((el) => (
-                <ExpenseItem
-                    key={el.id}
-                    title={el.title}
-                    amount={el.amount}
-                    date={el.date}
-                />
-            ))}
-        </Card>
+        <div>
+            <Card className="expenses">
+            <ExpensesFilter onYearChange={changeYearHandler} initialYear={chosenYear} />
+                {props.expenses.map((el) => (
+                    <ExpenseItem
+                        key={el.id}
+                        title={el.title}
+                        amount={el.amount}
+                        date={el.date}
+                    />
+                ))}
+            </Card>
+        </div>
     );
 }
 
